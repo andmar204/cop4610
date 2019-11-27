a497293f29723277813cb3f3363f8cb7cec6d9ba
Andre Marin <amari087@fiu.edu>
2019-11-27 16:44:46 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -14,8 +14,8 @@ done
 TESTSIZE=$(stat -c%s "$TESTFILE")
 TESTSIZE2=$(stat -c%s "$TESTFILE2")
 
-echo "We've now added $LISTLEN x 2 lines to the test file, $TESTFILE"
+echo "I added $LISTLEN x 2 lines to the test file, $TESTFILE"
 echo "This test file is now $TESTSIZE bytes."
 
-echo "We've also added $LISTLEN lines to a secondary test file, $TESTFILE2"
+echo "I also added $LISTLEN lines to a secondary test file, $TESTFILE2"
 echo "This alternate test file is now $TESTSIZE2 bytes."
